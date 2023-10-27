cb97ea60cc2a188246f883c31095637f93968a85
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2023-10-27 17:12:18 +0600

    Convert the code to render model into react

--- components/spline-model/spline.tsx
+++ components/spline-model/spline.tsx
@@ -1,29 +1,12 @@
 "use client"
 
 import React, { useEffect } from "react";
-import { Application } from '@splinetool/runtime';
+import Spline from '@splinetool/react-spline';
+
 
 const Model = () => {
-  useEffect(() => {
-    const canvas = document.getElementById('canvas3d');
-    if (canvas) {
-      const canvasElement = canvas as HTMLCanvasElement;
-      const app = new Application(canvasElement);
-      // app.load('https://prod.spline.design/8Fe6Zk7zZVKaUOiV/scene.splinecode');
-      // app.load('https://prod.spline.design/sXaGi19VGxPNUwVS/scene.splinecode');
-      app.load('https://prod.spline.design/8Fe6Zk7zZVKaUOiV/scene.splinecode');
-
-
-      return () => {
-        app.dispose();
-      };
-    }
-  }, []);
-
   return (
-    <div className="w-full">
-      <canvas id="canvas3d"></canvas>
-    </div>
+    <Spline scene="https://prod.spline.design/8Fe6Zk7zZVKaUOiV/scene.splinecode" />
   );
 }
 
